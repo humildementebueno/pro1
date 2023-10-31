ceb904862aa0b11fc483ab9b5c043ca4822a8edd
humildementebueno <themarshial@gmail.com>
2023-10-31 19:39:16 -0400

    agregue rutas

---
+++ carrera/carrera.js
@@ -0,0 +1,21 @@
+const carreraInfo = ()=>{
+    event.preventDefault();
+    console.log('INFO!!');
+    window.location.href = './informatica/informatica.html';
+    
+}
+const direccionEsta = ()=>{
+    event.preventDefault();
+    console.log('Esta!!');
+    window.location.href = './estadistica/estadistica.html';
+    
+    
+}
+const carreraMate = ()=>{
+    event.preventDefault();
+    console.log('Mate!!');
+    window.location.href = './matematica/matematica.html';
+
+
+}
+
